4d514fd7ae10abe382e25e9730966149e91df447
Silviu Tugulan <silviu.tugulan@epimorphics.com>
2016-08-09 16:48:42 +0100

    Example polygons

--- app/myApp.component.js
+++ app/myApp.component.js
@@ -13,41 +13,38 @@ angular
 			function MyApp($scope, $http, dataservice){
 				var vm = this;
 
-				vm.zoneOfInfluence = {};
-
-				//proj4.defs("urn:ogc:def:crs:EPSG::27594", "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs");
-
-				angular.extend($scope, $http, {
-	                uk: {
-	                    lat: 55.505,
-	                    lng: -4,
-	                    zoom:5
-	                },
-
-	                defaults: {
-	                    scrollWheelZoom: true
-                	},
-	            }),
-
-				getZoneOfInfluence();
-
-				function getZoneOfInfluence(){
-					return dataservice.getGmlData().then(function onSuccess(data){
-						vm.zoneOfInfluence = data;
-						return vm.zoneOfInfluence;
-					}, function onFailure(message){
-						return "Failed at MyApp.getZoneOfInfluence()" + message;
-					});
+				vm.geoJson = {
+				 	"type": "FeatureCollection",
+				 	"features": [
+				 		{	"type": "Feature",
+	         				"geometry": {
+	           					"type": "Polygon",
+			           			"coordinates": [
+		             				[ 
+		             				  	[100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+		              				  	[100.0, 1.0], [100.0, 0.0] 
+		              				]
+		             			]
+							}
+						},
+						
+						{	"type": "Feature",
+							"geometry": {
+								"type": "Polygon",
+								"coordinates": [
+									[
+										[101, 1], [103, 2], [104, 2], [101, 1]
+									]
+								]
+							}
+						}
+				 	]
 				}
-	            
-	            
-	            // Get the countries geojson data from a JSON
-			    $http.get("example.geo.json").success(function(data, status) {
-			        angular.extend($scope, {
-			            geojson: {
-			                data: data
-			            }
-			        });
+
+				angular.extend($scope, {
+		        	geojson: {
+		        		data: vm.geoJson	
+		        	}
 			    });
 			}
 		]
