2be03dc96c9f34b4fdc46e51b109b3f6004fa3bc
Silviu Tugulan <silviu.tugulan@epimorphics.com>
2016-08-12 14:14:35 +0100

    Spittal polygon

--- app/myApp.component.js
+++ app/myApp.component.js
@@ -8,11 +8,32 @@ angular
 		controller:
 		   ['$scope',
 		   	'$http',
+		   	'$log',
 		   	'dataservice',
 
-			function MyApp($scope, $http, dataservice){
+			function MyApp($scope, $http, $log, dataservice){
 				var vm = this;
 
+				//Define the used coordinate reference systems
+				proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs");
+
+				//Store the defined projections in variables
+				var firstProjection  = "EPSG:27700";
+				var secondProjection = "EPSG:4326";
+				
+				//Create a point by converting it from the first
+				//crs to the second
+				var point = proj4(firstProjection,secondProjection,[358835.572679,173560.671328]);
+				
+				//Display point values
+				// console.log(point[0]);
+				// console.log(point[1]);
+
+				vm.zoiGeoJson = {};
+
+				getZoiGeoJson();
+
+				//start
 				vm.geoJson = {
 				 	"type": "FeatureCollection",
 				 	"features": [
@@ -40,12 +61,28 @@ angular
 						}
 				 	]
 				}
+				
+				// vm.geoJson.features.push(vm.pointToAdd);
+				
 
-				angular.extend($scope, {
-		        	geojson: {
-		        		data: vm.geoJson	
-		        	}
-			    });
+				function getZoiGeoJson(){
+					return dataservice.getZoiGeoJson().then(function onSuccess(data){
+						vm.zoiGeoJson = data;
+						//console.log(vm.zoiGeoJson);
+						angular.extend($scope, {
+				        	geojson: {
+				        		data: vm.zoiGeoJson	
+				        	},
+
+				        	defaults: {
+					            scrollWheelZoom: true
+					        }
+					    });
+						return vm.zoiGeoJson;
+					}, function onFailure(message){
+						$log.error("Failed at `MyApp.getZoiGeoJson()` " + message);
+					})
+				}
 			}
 		]
 	});
--- app/myApp.dataservice.js
+++ app/myApp.dataservice.js
@@ -7,6 +7,9 @@ angular
 		[ 
 
 		function(){
+			
+			/* Provided funcionality */
+
 			var service = {
 				getZoneOfInfluence: getZoneOfInfluence
 			}
@@ -15,6 +18,8 @@ angular
 				"http://environment.data.gov.uk/doc/bathing-water";
 
 			return service;
+
+			/* Functions implementation */
 
 			function getZoneOfInfluence(){
 				return BATHING_WATERS_API_CALL + "?_pageSize=10&_properties=zoneOfInfluence.extent.asGML";
@@ -27,18 +32,102 @@ angular
 		 'apiservice', 
 
 		function($http, apiservice){
+			
+			/* Provided functionality */
+
 			var service = {
-				getGmlData: getGmlData
+				getZoiGeoJson: getZoiGeoJson
 			}
 
 			return service;
 
-			function getGmlData(){
+			
+			/* Functions implementation */
+
+			function getZoiGeoJson(){
 				var API_CALL = apiservice.getZoneOfInfluence();
 				
+				proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs");
+				//proj4.defs("EPSG:4326","+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees");
 
-				return $http.get(API_CALL).then(function onSuccess(data){
-					console.log(data);
+				return $http.get(API_CALL).then(function onSuccess(response){
+					var gml = response.data.result.items[0].zoneOfInfluence.extent.asGML;
+	
+					gml = gml.replace('srsName="os:BNG"', '');
+				   	
+				   	//Start wrapping
+				   	var newGml = wrap(gml);
+				   	//End wrapping
+
+				   	
+				   	//create the gml object
+				 	//var gmlOptions = {
+					//     featureType: "feature",
+					//     featureNS:   "http://example.com/feature"
+					// };
+					
+					var osbgProjection = new ol.proj.Projection({code: "EPSG:27700"});
+					var wsgProjection  = new ol.proj.Projection({code: "EPSG:4326"});
+
+					
+					//get gml format and read in features from gml string
+					var gmlFormat 	 = new ol.format.GML();
+				   	var gmlFeatures = gmlFormat.readFeatures(newGml)//, projection);
+
+				   	//get geojson format and write feature in geojson string
+				   	var formatOptions = {
+				   		defaultDataProjection: osbgProjection,
+				   		geometryName: "Polygon"
+				   	}
+
+				   	var featuresOptions = {
+				   		featureProjection: osbgProjection,
+				   		dataProjection: wsgProjection
+				   	}
+
+				   	var geojsonFormat = new ol.format.GeoJSON(formatOptions);
+				   	var geojsonFeatures = geojsonFormat.writeFeaturesObject(gmlFeatures, featuresOptions);//osbgProjection, wsgProjection);
+				   	//
+				   	
+				   	//console.log(geojsonFeaturesOsgb);
+				   	//console.log(ol.proj.transform(geojsonFeatures.features[0].geometry, osbgProjection, wsgProjection));
+				   	//gmlFeatures.setGeometry("Polygon");
+				   	//console.log(gmlFeatures.getGeometry());
+				   	 console.log(geojsonFeatures);
+
+					
+					return geojsonFeatures;
 				})
+			}
+
+			// Function to wrap the gml string into a 
+			// feature collection
+			function wrap(gml){
+				var openFeatureCollection  = '<gml:FeatureCollection xmlns:gml="http://www.opengis.net/gml" srsName="EPSG:27700">',
+					closeFeatureCollection = "</gml:FeatureCollection>";
+
+				var openFeatureMember  = "<gml:featureMember>",
+					closeFeatureMember = "</gml:featureMember>";
+
+				var openFeature  = '<feature:feature xmlns:feature="http://example.com/feature">',
+				 	closeFeature = "</feature:feature>";
+				
+				var openGeometryFeature  = '<gml:geometry>',
+					closeGeometryFeature = "</gml:geometry>";
+
+				var foo = "<feature:foo/>"
+
+				var result = openFeatureCollection             	   +  
+									openFeatureMember          	   + 
+										openFeature            	   +
+											openGeometryFeature    + 
+												gml                +
+										//		foo 			   +
+											closeGeometryFeature   +
+							 			closeFeature               + 
+							 		closeFeatureMember             +
+							 closeFeatureCollection;
+				
+				return result;
 			}
 	}]);
